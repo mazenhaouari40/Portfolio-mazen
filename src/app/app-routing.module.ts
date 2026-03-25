dcc99b3cf3d931c0db604f277319af81b9ef0344
Mazen4545* <mazenhaouari97@gmail.com>
2026-03-25 22:19:19 +0100

    msg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,16 @@ import { ContactComponent } from './components/home/contact/contact.component';
 import { BannerComponent } from './components/home/banner/banner.component';
 import { JobsComponent } from './components/home/jobs/jobs.component';
 import { ProyectsComponent } from './components/home/proyects/proyects.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
 
-  { path: '', component: BannerComponent },
-  {path: 'banner', component: BannerComponent},
+  { path: '', component: HomeComponent },
+  /*{path: 'banner', component: BannerComponent},
   {path: 'contact', component: ContactComponent},
   {path: 'job', component: JobsComponent},
   {path: 'project', component:ProyectsComponent },
-
-
-
-  {path: 'about', component: AboutComponent},
+  {path: 'about', component: AboutComponent},*/
 
   { path: '**', pathMatch: 'full', redirectTo: '/' },
 ];
