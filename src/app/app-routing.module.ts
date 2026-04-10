a3a95bdd42892c012b95d97874b6de9aae143af3
Mazen4545* <mazenhaouari97@gmail.com>
2026-04-10 22:46:53 +0200

    mise a jour de header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,19 @@ import { BannerComponent } from './components/home/banner/banner.component';
 import { JobsComponent } from './components/home/jobs/jobs.component';
 import { ProyectsComponent } from './components/home/proyects/proyects.component';
 import { HomeComponent } from './components/home/home.component';
+import { MoreProyectsComponent } from './components/home/more-proyects/more-proyects.component';
 
 const routes: Routes = [
 
-  { path: '', component: HomeComponent },
-  /*{path: 'banner', component: BannerComponent},
+  { path: '', component: BannerComponent },
+  {path: 'banner', component: BannerComponent},
   {path: 'contact', component: ContactComponent},
   {path: 'job', component: JobsComponent},
   {path: 'project', component:ProyectsComponent },
-  {path: 'about', component: AboutComponent},*/
+  {path: 'about', component: AboutComponent},
+  {path: 'moreproject', component: MoreProyectsComponent},
 
-  { path: '**', pathMatch: 'full', redirectTo: '/' },
+  //{ path: '**', pathMatch: 'full', redirectTo: '/' },
 ];
 
 @NgModule({
